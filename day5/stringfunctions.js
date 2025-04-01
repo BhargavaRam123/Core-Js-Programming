99fb673d168ac53b833b4bff6987fd654a7706bc
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-04-01 17:59:35 +0530

    Implement charCodeAt to get Unicode value of characters

--- day5/stringfunctions.js
+++ day5/stringfunctions.js
@@ -1,2 +1,9 @@
 const charAtExample = "Hello World";
 console.log(charAtExample.charAt(0)); // Output: "H"
+
+/**
+ * String.prototype.charCodeAt()
+ * Returns the Unicode value of the character at the specified index
+ */
+const charCodeAtExample = "Hello";
+console.log(charCodeAtExample.charCodeAt(0)); // Output: 72
