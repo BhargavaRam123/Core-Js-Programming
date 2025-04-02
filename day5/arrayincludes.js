02c255e7e004bb97b9a1517d4dea9a3a6b55d365
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-04-02 10:32:03 +0530

    day start

--- day5/arrayincludes.js
+++
@@ -1,6 +0,0 @@
-const numbers = [1, 2, 3, 4, 5];
-console.log(numbers.includes(3)); // true
-console.log(numbers.includes(6)); // false
-
-// Can specify start position
-console.log(numbers.includes(1, 1)); // false (start from index 1)
