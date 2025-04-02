94a98485fdd715f55594c26f65fc3f636523c8f9
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-04-02 10:46:31 +0530

    implemented foreach function to calculate sum

--- day6/main.js
+++ day6/main.js
@@ -10,3 +10,12 @@ console.log(obj.new)
 var fruits = ["Banana", "Orange", "Apple", "Mango"];
 console.log(Array.isArray(fruits)); // true
 console.log(Array.isArray("Hello")); // false
+
+//The forEach() method calls a function (a callback function) once for each element in an array, in order.
+var arr = [1,2,3,4,5,6,7,8,9,10]
+var sum = 0;
+arr.forEach(function(item){
+    sum += item;
+}
+)
+console.log(sum) 
