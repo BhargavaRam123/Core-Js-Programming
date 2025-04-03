55585b25c18b46fb12541e93f620977995c184b4
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-04-03 15:00:27 +0530

    Added example demonstrating destructuring and pass-by-reference behavior in nested object in JavaScript

--- day7/passbyref.js
+++ day7/passbyref.js
@@ -16,3 +16,19 @@ const obj4 = {
 }
 obj4.name = "james"
 console.log(obj3,obj4)
+
+//while using destructuring all the top level properties will get the copy but if there is any nested object the will be call by reference
+
+let rabbit = {
+    name:"Nivens",
+    coat:{
+        color:"white",
+        length:"short"
+    }
+}
+let anotherrabbit = {
+    ...rabbit
+}
+anotherrabbit.name = "Bob"
+anotherrabbit.coat.color = "Red"
+console.log(rabbit,anotherrabbit)
