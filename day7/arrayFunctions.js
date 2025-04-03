2db4926adda291a44f2b68436c2068fe7db08d0b
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-04-03 17:02:36 +0530

    Added example demonstrating the implementation of the filter function in JavaScript

--- day7/arrayFunctions.js
+++ day7/arrayFunctions.js
@@ -4,3 +4,10 @@ const squares = numbers.map(function(num) {
   return num * num;
 });
 console.log(squares); // [1, 4, 9, 16, 25]
+
+//implimenting filter function
+const numbers1 = [1, 2, 3, 4, 5, 6];
+const evenNumbers = numbers1.filter(function(num) {
+  return num % 2 === 0;
+});
+console.log(evenNumbers); // [2, 4, 6]
