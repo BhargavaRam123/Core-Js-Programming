cf1f395275230adca6bac577ca0fc2f549719b7d
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-04-03 12:32:41 +0530

    Added example demonstrating object reference and spread operator behavior in JavaScript

--- day7/passbyref.js
+++ day7/passbyref.js
@@ -5,3 +5,14 @@ let obj2 = obj
 obj2.name = "world"
 obj2.age = 20
 console.log(obj,obj2)
+
+//trying to pass it without a reference 
+let obj3 = {
+    name:"jack",
+    age:20
+}
+const obj4 = {
+    ...obj3
+}
+obj4.name = "james"
+console.log(obj3,obj4)
