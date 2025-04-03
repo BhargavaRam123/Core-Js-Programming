916b72bf0019a6f01277e1801d620546858c0033
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-04-03 15:26:31 +0530

    Added examples demonstrating object modification and reassignment in JavaScript

---
+++ day7/objects.js
@@ -0,0 +1,21 @@
+let person ={
+    name:"steve"
+}
+function modifyobj(obj){
+    obj.name = "sam"
+}
+console.log(person)
+modifyobj(person)
+console.log(person)
+
+let person1={
+    name:"john"
+}
+function reassignobject(person1){
+    person1 = {
+        name:"bhargav"
+    }
+}
+console.log(person1)
+reassignobject(person1)
+console.log(person1)
