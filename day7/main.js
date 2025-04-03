34860c2fcace5e8f0bcdbe67169376c82f4ae300
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-04-03 10:48:04 +0530

    Added implementation of WeakSet with example usage

--- day7/main.js
+++ day7/main.js
@@ -6,3 +6,14 @@ console.log(wm.get(obj))
 console.log(wm)
 obj = null
 //the entry in the weakmap of the corresponding key will be garbage collected
+
+//implementation of weakset
+let ws = new WeakSet()
+let obj1 = {}
+let obj2 = {}
+ws.add(obj1)
+ws.add(obj2)
+console.log(ws.has(obj1)) //true
+console.log(ws.has(obj2)) //true
+obj1 = null
+console.log(ws.has(obj1)) //false
