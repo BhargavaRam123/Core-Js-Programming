3af434d78059732c59cb847b13f505ce288a9794
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-04-03 15:34:07 +0530

    Added example demonstrating array manipulation and pass-by-reference behavior in JavaScript

--- day7/objects.js
+++ day7/objects.js
@@ -19,3 +19,11 @@ function reassignobject(person1){
 console.log(person1)
 reassignobject(person1)
 console.log(person1)
+
+let arr =[1,2]
+function arraymanipulation( arr){
+    arr[0]=10
+}
+console.log(arr)
+arraymanipulation(arr)
+console.log(arr)
