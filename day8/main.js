f925aa5f3321a0cc53576fc18d2c4d5e6c7dd158
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-04-04 11:35:49 +0530

    Enhance symbol usage in main.js with examples for debugging

--- day8/main.js
+++ day8/main.js
@@ -9,3 +9,16 @@ const s = Symbol("property")
 let obj = {}
 obj[s] = "my value"
 console.log(obj)
+
+//example on using symbol
+const person ={
+    role:"sde"
+}
+//by providing the parameter value it will be helpfull during the time of debugging
+const nsymb = Symbol("name")
+const asymb = Symbol("age")
+person[nsymb] = "john"
+person[asymb] = 20
+
+console.log(person)
+console.log(person[nsymb])
