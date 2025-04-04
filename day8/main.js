8e18dc4621b5c1954a1e32ffdb06479893bd745b
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-04-04 11:28:02 +0530

    Implement usage of symbols for unique object properties in main.js

--- day8/main.js
+++ day8/main.js
@@ -2,3 +2,10 @@
 const symbl = Symbol()
 const symbl1 = Symbol()
 console.log(symbl===symbl1)
+
+//using symbols for unique properties in objects
+
+const s = Symbol("property")
+let obj = {}
+obj[s] = "my value"
+console.log(obj)
